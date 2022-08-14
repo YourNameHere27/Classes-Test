77d538741f46809822fae1d80b1547323abf9d0a
YourNameHere27 <107463368+YourNameHere27@users.noreply.github.com>
2022-08-14 19:54:41 +0100

    Add files via upload

---
+++ class_teste/Classe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace class_teste
+{
+    internal class Classe
+    {
+        public void Ataque1()
+        {
+            if (progressBar1.Value - 25 <= 0)
+            {
+                progressBar1.Value = progressBar1.Value - 25;
+            }
+            else
+            {
+                progressBar1.Value = 0;
+            }
+        }
+
+        public void Ataque2()
+        {
+            if (progressBar1.Value - 50 <= 0)
+            {
+                progressBar1.Value = progressBar1.Value - 50;
+            }
+            else
+            {
+                progressBar1.Value = 0;
+            }
+        }
+
+        public void Curar()
+        {
+            progressBar1.Value = progressBar1.Maximum;
+        }
+    }
+}
---
+++ class_teste/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Maximum = 100;
+            progressBar1.Value = progressBar1.Maximum;
+
+            label1.Text = progressBar1.Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Classe acao1 = new Classe();
+            acao1.Ataque1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Classe acao2 = new Classe();
+            acao2.Ataque2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Classe acao3 = new Classe();
+            acao3.Curar();
+        }
+    }
+}
